51776fa9e49eb1bedf1343ebff0998dcb7e4b273
Vlad Vovk <wolfykey@gmail.com>
2024-06-29 15:34:20 +0200

    done

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let fruits = vec!["banana", "apple", "coconut", "orange", "strawberry"];
+    for &index in [0, 2, 99].iter() {
+        match fruits.get(index) {
+            Some(&"coconut") => println!("Coconuts are awesome!!!"),
+            Some(fruit_name) => println!("It's a delicious {}!", fruit_name),
+            None => println!("There is no fruit! :("),
+        }
+    }
 }
