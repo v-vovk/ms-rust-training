3a30334df051a71b00ee5f0e502d6357c3f4e277
Vlad Vovk <wolfykey@gmail.com>
2024-07-06 15:31:28 +0200

    done

--- generic-implementation/src/main.rs
+++ generic-implementation/src/main.rs
@@ -1,9 +1,9 @@
-struct Container {
-    value: u32,
+struct Container<T> {
+    value: T,
 }
 
-impl Container {
-    pub fn new(value: u32) -> Self {
+impl<T> Container<T> {
+    pub fn new(value: T) -> Self {
         Container { value }
     }
 }
@@ -12,7 +12,10 @@ fn main() {
     assert_eq!(Container::new(42).value, 42);
     assert_eq!(Container::new(3.14).value, 3.14);
     assert_eq!(Container::new("Foo").value, "Foo");
-    assert_eq!(Container::new(String::from("Bar")).value, String::from("Bar"));
+    assert_eq!(
+        Container::new(String::from("Bar")).value,
+        String::from("Bar")
+    );
     assert_eq!(Container::new(true).value, true);
     assert_eq!(Container::new(-12).value, -12);
     assert_eq!(Container::new(Some("text")).value, Some("text"));
