7ab441c76de6b4767f9392768cb87a3be8ed6795
Vlad Vovk <wolfykey@gmail.com>
2024-07-06 15:27:59 +0200

    init

---
+++ generic-implementation/src/main.rs
@@ -0,0 +1,19 @@
+struct Container {
+    value: u32,
+}
+
+impl Container {
+    pub fn new(value: u32) -> Self {
+        Container { value }
+    }
+}
+
+fn main() {
+    assert_eq!(Container::new(42).value, 42);
+    assert_eq!(Container::new(3.14).value, 3.14);
+    assert_eq!(Container::new("Foo").value, "Foo");
+    assert_eq!(Container::new(String::from("Bar")).value, String::from("Bar"));
+    assert_eq!(Container::new(true).value, true);
+    assert_eq!(Container::new(-12).value, -12);
+    assert_eq!(Container::new(Some("text")).value, Some("text"));
+}
